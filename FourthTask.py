1c81abcefdd063b7f6391ba3b7aa6b07e0a5df01
F1zznt <oleg88724@gmail.com>
2024-03-06 16:30:19 +0300

    Задача 4 от Попова Олега. 06.03.2024. 16:30

--- FourthTask.py
+++ FourthTask.py
@@ -1,1 +1,50 @@
 # Добавьте переключатель слоёв карты (схема/спутник/гибрид), при изменении которого надо менять вид карты.
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import requests
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 410)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(430, 360, 161, 41))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(220, 360, 161, 41))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(10, 360, 161, 41))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 601, 351))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_3.setText(_translate("MainWindow", "Гибрид"))
+        self.pushButton_4.setText(_translate("MainWindow", "Гибрид"))
+        self.pushButton_5.setText(_translate("MainWindow", "Гибрид"))
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+response = requests.get("https://static-maps.yandex.ru/1.x/?ll=37.617901,55.751434&spn=0.01,0.01&l=map&size=601,351")
+with open("map.jpeg", "wb") as jpeg:
+    jpeg.write(response.content)
+MainWindow.show()
+sys.exit(app.exec_())
+
